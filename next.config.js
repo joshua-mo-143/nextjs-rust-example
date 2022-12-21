bf0ad885479e19044a5181da9163595e73ba4bdb
Joshua Mo <102877324+joshua-mo-143@users.noreply.github.com>
2022-12-21 22:52:55 +0000

    + changed next.js config to allow html export + add static file folder in rust webserver

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    unoptimized: true
+  },
+  trailingSlash: true
 }
-
-module.exports = nextConfig
--- shuttle-next/src/lib.rs
+++ shuttle-next/src/lib.rs
@@ -1,16 +1,33 @@
-use axum::{routing::get, Router};
+use axum::{Router};
 use sync_wrapper::SyncWrapper;
+use std::path::PathBuf;
+use axum_extra::routing::SpaRouter;
 
-async fn hello_world() -> &'static str {
-    "Hello, world!"
+// a data structure for designing what the app state should look like
+struct AppState {
+    public: PathBuf
 }
 
 #[shuttle_service::main]
-async fn axum(#[shuttle_static_folder::StaticFolder(folder = "public")] public_folder: PathBuf)
+async fn axum(#[shuttle_static_folder::StaticFolder(folder = "out")] public_folder: PathBuf)
              -> shuttle_service::ShuttleAxum {
 
-    let router = Router::new().route("/", get(public_folder.test));
+    // initialise an AppState struct that can be used across any route
+    // This is basically Rust's equivalent of global state in React/next etc
+    let app = AppState{public: public_folder};
+
+    // initialise the router using the spa router function
+    let router = spa_router(&app);
     let sync_wrapper = SyncWrapper::new(router);
 
     Ok(sync_wrapper)
+}
+
+fn spa_router(app: &AppState) -> Router {
+    // `SpaRouter` is the easiest way to serve assets at a nested route like `/assets`
+    //
+    // Requests starting with `/assets` will be served from files in the current directory.
+    // Requests to unknown routes will get `index.html`.
+    Router::new()
+        .merge(SpaRouter::new("/", &app.public).index_file("index.html"))
 }
