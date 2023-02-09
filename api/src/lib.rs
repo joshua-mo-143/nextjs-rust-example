714b6539c8589bd0d9e60f0aa25049323d523d9c
Joshua Mo <102877324+joshua-mo-143@users.noreply.github.com>
2023-02-09 19:30:11 +0000

    add a meme

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -10,8 +10,8 @@ async fn axum(#[shuttle_static_folder::StaticFolder(folder = "assets")] public_f
     // initialise the router
     // add a health check route to make sure the api works
     let router = Router::new()
-    .route("/api/health", get(health_check));
-    // .merge(SpaRouter::new("/", &public_folder).index_file("index.html"));
+    .route("/api/health", get(health_check))
+    .merge(SpaRouter::new("/", &public_folder).index_file("index.html"));
 
     let sync_wrapper = SyncWrapper::new(router);
 
